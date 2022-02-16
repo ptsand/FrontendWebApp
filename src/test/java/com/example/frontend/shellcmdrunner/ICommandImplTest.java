bdf5b1e74fa543818aa92b30782e815d6b885f45
ptsand <ptrsand@gmail.com>
2022-02-16 16:55:15 +0100

    Merge

---
+++ src/test/java/com/example/frontend/shellcmdrunner/ICommandImplTest.java
@@ -0,0 +1,29 @@
+package com.example.frontend.shellcmdrunner;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+class ICommandImplTest {
+
+    @Test
+    void iCommandImplTest() throws IOException {
+        List<String> cmd = new ArrayList<>();
+        ICommand iCmd = new ICommandImpl();
+        // Create command depending on OS - TODO: check exit status
+        if(System.getProperty("os.name").toLowerCase().contains("win")) {
+            cmd.add("cmd.exe");
+            cmd.add("/c");
+            cmd.add("dir");
+            cmd.add("/a");
+        } else { // Assert linux environment if not windows
+            cmd.add("sh"); // posix compliant shell
+            cmd.add("-c"); // run cmd
+            cmd.add("ls"); // cmd to run
+            cmd.add("-l"); // arg
+        }
+        System.out.println(iCmd.runShellCmd(cmd));
+    }
+}
